8c45757f5222158be11afbfb728284c7a0430e93
hhduser <github@aliensoftware.dev>
2019-05-30 08:53:47 +0700

    [FIX] as_filter_selection: When values are changed, the selection must be updated - resetOnAnyFieldChange

--- as_filter_selection/static/src/js/widget.js
+++ as_filter_selection/static/src/js/widget.js
@@ -8,6 +8,7 @@ odoo.define('alsw_filter_selection.dynamic_selection', function(require) {
     var pyUtils = require('web.py_utils');
 
     var DynamicSelection = relational_fields.FieldSelection.extend({
+        resetOnAnyFieldChange: true,
 
         _dynamic_selection: function(){
             if('alsw' in this.attrs){
@@ -15,7 +16,7 @@ odoo.define('alsw_filter_selection.dynamic_selection', function(require) {
                 for(var i=0 ; i < alsw.length; i++){
                     var filter = alsw[i];
                     if('domain' in filter && 'values' in filter){
-                        var check = new Domain(filter.domain).compute(this.save_point);
+                        var check = new Domain(filter.domain).compute(this.record.data);
                         if(check){
                             var keep_values = filter.values;
                             var default_values = this.values;
@@ -32,14 +33,14 @@ odoo.define('alsw_filter_selection.dynamic_selection', function(require) {
             }
         },
 
-        init: function (){
+        _reset: function() {
             this._super.apply(this, arguments);
-            this.save_point = this.record.data;
+            this._dynamic_selection();
+            this.reset_el = this.$el;
         },
 
-        _renderEdit: function() {
+        _renderEdit: function (){
             this._super.apply(this, arguments);
-            this.$el.addClass('oe_dynamic_selection');
             this._dynamic_selection();
         },
 
